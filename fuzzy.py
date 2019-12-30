520e58eb17de412982268d67ca9b65d0c57df369
Robotmurlock <momir.adzemovic@gmail.com>
2019-12-30 17:14:31 +0100

    Implemented Fuzzy System class and simple Fuzzy Logic for car autopilot

---
+++ fuzzy.py
@@ -0,0 +1,261 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+from enum import Enum, unique
+@unique
+class Logic(Enum):
+    OR = 0
+    AND = 1
+
+class MFInput:
+    def __init__(self, name, xs, ys):
+        self.name = name
+        self.points = np.array(list(zip(xs, ys)))
+        self.size = xs.size
+        self.x0 = None
+        self.mi = None
+    
+    def setMi(self, x0):
+        self.x0 = x0
+        self.mi = self.getMi()
+        
+    def getY(self, x1, y1, x2, y2):
+        if y1 == y2:
+            return y1
+        if y1 < y2:
+            return (self.x0 - x1) / (x2 -x1)
+        return (x2 - self.x0) / (x2 - x1)
+        
+    def getMi(self):
+        if self.x0 < self.points[0][0]: 
+            return self.points[0][1]
+        if self.x0 > self.points[-1][0]:
+            return self.points[-1][1]
+        for i in range(1, self.size):
+            x1 = self.points[i - 1][0]
+            x2 = self.points[i][0]
+            if self.x0 >= x1 and self.x0 < x2:
+                y1 = self.points[i - 1][1]
+                y2 = self.points[i][1]
+                return self.getY(x1, y1, x2, y2)
+        return None
+        
+    def __str__(self):
+        out = '['
+        for (x, y) in self.points:
+            out = out + '(' + str(x) + ',' + str(y) + '), '
+        out = out[:-2] + ']'
+        return out
+    
+    def show_diagram(self):
+        xs = [p[0] for p in self.points]
+        ys = [p[1] for p in self.points]
+        
+        plt.ylim(-0.1, 1.4)
+        plt.xlabel('input value')
+        plt.ylabel(self.name + ' value')
+        plt.title('Fuzzy Diagram')
+        plt.plot(xs, ys, 'k')
+        
+        plt.plot([self.x0, self.x0], [0, self.mi], 'ro--')
+
+class MFOutput:
+    def __init__(self, name, xs, ys):
+        self.name = name
+        self.points = np.array(list(zip(xs, ys)))
+        self.size = xs.size
+        
+        self.center = None
+        sum1 = 0
+        cnt1 = 0
+        for i in range(0, self.size):
+            if ys[i] == 1:
+                sum1 = sum1 + xs[i]
+                cnt1 = cnt1 + 1
+        self.center = sum1/cnt1
+        self.mi = 0
+        
+    def show_diagram(self, solution):
+        xs = [p[0] for p in self.points]
+        ys = [p[1] for p in self.points]
+        
+        plt.ylim(-0.1, 1.4)
+        plt.xlabel('output')
+        plt.ylabel(self.name + ' value')
+        plt.title('Fuzzy Diagram')
+        plt.plot(xs, ys, 'k')
+        
+        plt.plot([solution, solution], [0, 1], 'ro--')
+
+class Rule:
+    def __init__(self, inputs, output, operator = Logic.AND):
+        self.inputs = inputs
+        self.output = output
+        self.operator = operator
+    
+    def apply_rule(self):
+        n = self.inputs.size
+        if self.operator == Logic.OR:
+            mi = 0
+            for i in range(0, n):
+                mi = max(mi, self.inputs[i].mi)
+        else:
+            mi = 1
+            for i in range(0, n):
+                mi = min(mi, self.inputs[i].mi)
+                
+        self.output.mi = max(self.output.mi, mi)
+        
+    def __str__(self):
+        n = self.inputs.size
+        out = ''
+        for i in range(0, n-1):
+            out = out + self.inputs[i].name + ' ' + str(self.operator) + ' '
+        out = out + self.inputs[n-1].name
+        return out
+
+class FuzzyInput:
+    def __init__(self, name, inputs):
+        self.name = name
+        self.inputs = inputs
+        self.size = self.inputs.size
+    
+    def __getitem__(self, index):
+         return self.inputs[index]
+        
+    def __str__(self):
+        return self.name + " : " + str(self.inputs)
+    
+    def show_diagram(self):
+        for i in range(0, self.size):
+            self.inputs[i].show_diagram()
+        plt.ylabel(self.name)
+        plt.show()
+
+class FuzzyOutput:
+    def __init__(self, name, outputs):
+        self.name = name
+        self.outputs = outputs
+        self.size = self.outputs.size
+    
+    def __getitem__(self, index):
+         return self.outputs[index]
+        
+    def __str__(self):
+        return self.name + " : " + str(self.outputs)
+    
+    def show_diagram(self, solution):
+        for i in range(0, self.size):
+            self.outputs[i].show_diagram(solution)
+        plt.ylabel(self.name)
+        plt.show()
+
+class FuzzyRules:
+    def __init__(self, rules):
+        self.rules = rules
+        self.size = self.rules.size
+        self.size = self.rules.size
+    
+    def __getitem__(self, index):
+         return self.rules[index]
+        
+    def __str__(self):
+        out = ''
+        for i in range(0, self.size):
+            out = out + str(self.rules[i]) + '\n'
+        return out
+
+class FuzzySystem:
+    def __init__(self, inputs, output, rules):
+        self.inputs = inputs
+        self.output = output
+        self.rules = rules
+        self.solution = None
+        
+    def fit(self, x0s):
+        for i in range(0, self.inputs.size):
+            x0 = x0s[i]
+            for j in range(0, self.inputs[i].size):
+                self.inputs[i][j].setMi(x0)
+            
+        for i in range(0, self.rules.size):
+            self.rules[i].apply_rule()
+            
+        numerator = 0
+        denominator = 0 
+        for mfo in self.output:
+            numerator += mfo.mi * mfo.center
+            denominator += mfo.mi
+        if denominator == 0:
+            self.solution = 0
+        else:
+            self.solution = numerator/denominator
+        
+    def inputs_info(self):
+        if self.solution == None:
+            return
+        for i in range(0, self.inputs.size):
+            self.inputs[i].show_diagram()
+            
+    def output_info(self):
+        if self.solution == None:
+            return
+        self.output.show_diagram(self.solution)
+        
+    def full_info(self):
+        self.inputs_info()
+        self.output_info()
+
+if __name__ == "__main__":
+    left_sensor = FuzzyInput("left_sensor", np.array([
+        MFInput("close", np.array([0, 8]), np.array([1, 0])),
+        MFInput("midrange", np.array([6, 10, 12, 14]), np.array([0, 1, 1, 0])),
+        MFInput("far", np.array([12, 20]), np.array([0, 1])),
+    ]))
+
+    right_sensor = FuzzyInput("right_sensor", np.array([
+        MFInput("close", np.array([0, 8]), np.array([1, 0])),
+        MFInput("midrange", np.array([6, 10, 12, 14]), np.array([0, 1, 1, 0])),
+        MFInput("far", np.array([12, 20]), np.array([0, 1])),
+    ]))
+
+    front_sensor = FuzzyInput("front_sensor", np.array([
+        MFInput("close", np.array([0, 8]), np.array([1, 0])),
+        MFInput("midrange", np.array([6, 10, 12, 14]), np.array([0, 1, 1, 0])),
+        MFInput("far", np.array([12, 20]), np.array([0, 1])),
+    ]))
+
+    velocity = FuzzyOutput("velocity", np.array([
+        MFOutput("low", np.array([0, 10]), np.array([1, 0])),
+        MFOutput("middle", np.array([8, 10, 12, 16]), np.array([0, 1, 1, 0])),
+        MFOutput("high", np.array([14, 20]), np.array([0, 1])),
+    ]))
+
+    angle = FuzzyOutput("angle", np.array([
+        MFOutput("left", np.array([120, 180]), np.array([1, 0])),
+        MFOutput("forward", np.array([50, 90, 130]), np.array([0, 1, 0])),
+        MFOutput("right", np.array([0, 60]), np.array([0, 1])),
+    ]))
+
+    angle_rules = FuzzyRules(np.array([
+        Rule(np.array([left_sensor[0]]), angle[2]),
+        Rule(np.array([right_sensor[0]]), angle[0])
+    ]))
+
+    velocity_rules = FuzzyRules(np.array([
+        Rule(np.array([front_sensor[2]]), velocity[2]),
+        Rule(np.array([front_sensor[0]]), velocity[0])
+    ]))
+
+    FSAngle = FuzzySystem(np.array([left_sensor, front_sensor, right_sensor]), angle, angle_rules)
+    FSVelocity = FuzzySystem(np.array([left_sensor, front_sensor, right_sensor]), velocity, velocity_rules)
+
+    FSInput = np.array([3, 30, 15])
+    FSAngle.fit(FSInput)
+    FSVelocity.fit(FSInput)
+    print('Angle in degrees: ' + str(FSAngle.solution))
+    print('Velocity: ' + str(FSVelocity.solution))
+
+    FSAngle.full_info()
+    FSVelocity.full_info()
+    
