146ece7c8bc7b99b590c54fede1e921596411bf9
Aleksa Tesic <aleksatesicteske@gmail.com>
2020-01-12 16:12:40 +0100

    Update convex polygon generation

--- utils/path_generator.py
+++ utils/path_generator.py
@@ -7,7 +7,7 @@ import utils.linear_transformations as lt
 from utils import constants
 
 def generate_convex_polygon(): 
-    coords = []
+    coords = [(constants.CAR_POS_X, constants.CAR_POS_Y + 10)]
     num_of_points = 20
     
     for i in range (num_of_points):
@@ -17,10 +17,11 @@ def generate_convex_polygon():
 
     coords1 = ch.gift_wrap(coords)
 
-    scaling_factor = 0.8
-    offset = 75
+    scaling_factor = 0.6
+    offset_x = 200
+    offset_y = 150
     coords2 = lt.apply_scaling(scaling_factor, scaling_factor, coords1)
-    coords2 = lt.apply_translation(offset, offset, coords2)
+    coords2 = lt.apply_translation(offset_x, offset_y, coords2)
 
     polygon = [coords1, coords2]
     
